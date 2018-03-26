8eccc87bec25300f3209323f7ed564213fc435a1
Administrator <Administrator@10.6.84.22>
2018-03-26 15:37:58 +0300

    Comm2

--- src/examples/Antenna.java
+++ src/examples/Antenna.java
@@ -27,7 +27,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
-// zaki Alsubhi
+// zaki Alsubhi CPIT251
 package examples;
 
 public class Antenna extends javax.swing.JFrame {
